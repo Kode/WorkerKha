a13671cf7537ae506adf93f0044006620bb2d722
Robert Konrad <robert@ktx-software.com>
2015-01-03 12:41:21 +0100

    Improved graphics, added keyboard support.

--- Sources/WorkerKha.hx
+++ Sources/WorkerKha.hx
@@ -6,24 +6,36 @@ import kha.Color;
 import kha.Framebuffer;
 import kha.Game;
 import kha.Image;
+import kha.input.Keyboard;
+import kha.Key;
 import kha.Loader;
 import kha.math.Matrix3;
 import kha.Music;
 import kha.Sound;
+import kha.Starter;
+
+class Frame {
+	public var commands: Array<Dynamic>;
+	
+	public function new() {
+		commands = [];
+	}
+}
 
 class WorkerKha extends Game {
 	private var worker: Worker;
-	private var renderCommands: Array<Dynamic>;
-	private var lastRenderEndIndex: Int;
+	private var frames: Array<Frame>;
+	private var currentFrame: Frame;
 	private var images: Map<Int, Dynamic>;
 	private var lastImageId: Int;
 	
 	public function new() {
 		super("WorkerKha", false);
-		renderCommands = [];
+		frames = [];
+		currentFrame = new Frame();
 		images = new Map();
 		lastImageId = 0;
-		lastRenderEndIndex = -1;
+		Keyboard.get().notify(keyboardDown, keyboardUp);
 	}
 	
 	override public function loadFinished(): Void {
@@ -34,22 +46,40 @@ class WorkerKha extends Game {
 	
 	override public function render(frame: Framebuffer): Void {
 		super.render(frame);
-		var g = frame.g2;
-		for (command in renderCommands) {
-			switch (command.command) {
-			case 'drawImage':
-				g.transformation = Matrix3.identity();
-				g.color = Color.White;
-				g.drawImage(images[command.id], command.x, command.y);
-			case 'drawScaledSubImage':
-				g.transformation = Matrix3.identity();
-				g.color = Color.White;
-				g.drawScaledSubImage(images[command.id], command.sx, command.sy, command.sw, command.sh, command.dx, command.dy, command.dw, command.dh);
-			case 'end':
-				break;
+		if (frames.length > 0) {
+			var g = frame.g2;
+			g.begin();
+			var lastFrame = frames[frames.length - 1]; 
+			var commands = lastFrame.commands;
+			for (command in commands) {
+				switch (command.command) {
+				case 'drawImage':
+					g.color = Color.White;
+					g.drawImage(images[command.id], command.x, command.y);
+				case 'drawScaledSubImage':
+					g.color = Color.White;
+					g.drawScaledSubImage(images[command.id], command.sx, command.sy, command.sw, command.sh, command.dx, command.dy, command.dw, command.dh);
+				case 'setTransformation':
+					g.transformation = new Matrix3([command._0, command._1, command._2, command._3, command._4, command._5, command._6, command._7, command._8]);
+				case 'end':
+					break;
+				}
 			}
+			g.end();
+			frames = [];
+			frames.push(lastFrame);
 		}
 		worker.postMessage( { command: 'frame' } );
+	}
+	
+	private function keyboardDown(key: Key, char: String): Void {
+		super.keyDown(key, char);
+		worker.postMessage( { command: 'keyDown', key: key.getIndex(), char: char } );
+	}
+	
+	private function keyboardUp(key: Key, char: String): Void {
+		super.keyUp(key, char);
+		worker.postMessage( { command: 'keyUp', key: key.getIndex(), char: char } );
 	}
 	
 	private function onMessage(message: Dynamic): Void {
@@ -74,13 +104,14 @@ class WorkerKha extends Game {
 				worker.postMessage( { command: 'loadedMusic', file: data.file } );
 			});
 		case 'drawImage':
-			renderCommands.push( { command: 'drawImage', id: data.id, x: data.x, y: data.y } );
+			currentFrame.commands.push( { command: 'drawImage', id: data.id, x: data.x, y: data.y } );
 		case 'drawScaledSubImage':
-			renderCommands.push( { command: 'drawScaledSubImage', id: data.id, sx: data.sx, sy: data.sy, sw: data.sw, sh: data.sh, dx: data.dx, dy: data.dy, dw: data.dw, dh: data.dh } );
+			currentFrame.commands.push( { command: 'drawScaledSubImage', id: data.id, sx: data.sx, sy: data.sy, sw: data.sw, sh: data.sh, dx: data.dx, dy: data.dy, dw: data.dw, dh: data.dh } );
+		case 'setTransformation':
+			currentFrame.commands.push( { command: 'setTransformation', _0: data._0, _1: data._1, _2: data._2, _3: data._3, _4: data._4, _5: data._5, _6: data._6, _7: data._7, _8: data._8 } );
 		case 'end':
-			if (lastRenderEndIndex > 0) renderCommands.splice(0, lastRenderEndIndex + 1);
-			renderCommands.push( { command: 'end' } );
-			lastRenderEndIndex = renderCommands.length - 1;
+			frames.push(currentFrame);
+			currentFrame = new Frame();
 		}
 	}
 }
